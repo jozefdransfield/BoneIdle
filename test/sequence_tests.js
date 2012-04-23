20615ce8288dfa4796244994ad5c03bb404df8fe
Jozef Dransfield <Jozef.Dransfield@me.com>
2012-04-23 20:26:04 +0100

    Removed error from the callback

--- test/sequence_tests.js
+++ test/sequence_tests.js
@@ -11,104 +11,104 @@ module.exports = {
         callback();
     },
     "Realise Returns Array For Sequence Initialised with Array":function (test) {
-        b_.sequence([1, 2, 3]).realise(function (err, value) {
+        b_.sequence([1, 2, 3]).realise(function (value) {
             test.same(value, [1, 2, 3]);
             test.done()
         });
     },
     "Realise Returns Array For Sequence Initialised with arguements":function (test) {
-        b_.sequence(1, 2, 3).realise(function (err, value) {
+        b_.sequence(1, 2, 3).realise(function (value) {
             test.same(value, [1, 2, 3]);
             test.done()
         });
     },
     "Realise Returns Array For Sequence Initialised with object":function (test) {
-        b_.sequence({a:1, b:2, c:3}).realise(function (err, value) {
+        b_.sequence({a:1, b:2, c:3}).realise(function (value) {
             test.same(value, [1, 2, 3]);
             test.done()
         });
     },
     "Realise Returns Array For Sequence Initialised with Iterator":function (test) {
-        b_.sequence(dummyIterator()).take(1, function (err, value) {
+        b_.sequence(dummyIterator()).take(1, function (value) {
             test.same(value, [1]);
             test.done()
         });
     },
     "Map Function":function (test) {
-        b_.sequence([1, 2, 3]).map(multiplyBy100).realise(function (err, value) {
+        b_.sequence([1, 2, 3]).map(multiplyBy100).realise(function (value) {
             test.same(value, [100, 200, 300]);
             test.done();
         });
     },
     "Fold Function":function (test) {
-        b_.sequence([1, 2, 3]).foldLeft(0, sum, function (err, value) {
+        b_.sequence([1, 2, 3]).foldLeft(0, sum, function (value) {
             test.equals(value, 6);
             test.done();
         });
     },
     "Head Returns Empty Option on Empty Array":function (test) {
-        b_.sequence([]).head(function (err, value) {
+        b_.sequence([]).head(function (value) {
             test.ok(value.isEmpty);
             test.done();
         });
     },
     "Head Returns Empty Option on Empty Sequence":function (test) {
-        b_.sequence().head(function (err, value) {
+        b_.sequence().head(function (value) {
             test.ok(value.isEmpty);
             test.done();
         });
     },
     "Filter Function":function (test) {
-        b_.sequence(1, 2, 3, 4).filter(even).realise(function (err, value) {
+        b_.sequence(1, 2, 3, 4).filter(even).realise(function (value) {
             test.same(value, [2, 4]);
             test.done();
         });
     },
     "Find Function":function (test) {
-        b_.sequence(1, 2, 3, 4).find(even, function (err, option) {
+        b_.sequence(1, 2, 3, 4).find(even, function (option) {
             test.equals(option.get(), 2)
             test.done()
         })
     },
     "Take X From sequence":function (test) {
-        b_.sequence(1, 2, 3, 4).take(2, function (err, value) {
+        b_.sequence(1, 2, 3, 4).take(2, function (value) {
             test.same(value, [1,2])
             test.done()
         })
     },
     "Take While From sequence":function (test) {
-        b_.sequence(1, 2, 3, 4).takeWhile(lessThan3, function (err, value) {
+        b_.sequence(1, 2, 3, 4).takeWhile(lessThan3, function (value) {
             test.same(value, [1,2])
             test.done()
         })
     },
     "Find Function with No Matches Returns Empty Option":function (test) {
-        b_.sequence(1, 3, 5, 7).find(even, function (err, option) {
+        b_.sequence(1, 3, 5, 7).find(even, function (option) {
             test.ok(option.isEmpty)
             test.done();
         });
     },
     "Read File Then Map":function (test) {
-        b_.stream(fs.ReadStream("./test/sample.txt", {encoding:"utf8"})).map(addNewLine).realise(function (err, data) {
+        b_.stream(fs.ReadStream("./test/sample.txt", {encoding:"utf8"})).map(addNewLine).realise(function (data) {
             console.log("I finished!")
             test.same(data, ["Sample File Data\n"]);
             test.done();
         })
     },
     "Read File then Filter":function (test) {
-        b_.stream(fs.ReadStream("./test/sample.txt", {encoding:"utf8"})).filter(allways).realise(function (err, data) {
+        b_.stream(fs.ReadStream("./test/sample.txt", {encoding:"utf8"})).filter(allways).realise(function (data) {
             test.same(data, ["Sample File Data"]);
             test.done();
         })
     },
     "Read File Stream returns contents":function (test) {
-        b_.stream(fs.ReadStream("./test/sample.txt", {encoding:"utf8"})).realise(function (err, data) {
+        b_.stream(fs.ReadStream("./test/sample.txt", {encoding:"utf8"})).realise(function (data) {
             test.same(data, ["Sample File Data"]);
             test.done();
         });
     },
     "Read Invalid File Stream returns error":function (test) {
-        b_.stream(fs.ReadStream("./test/badfilename", {encoding:"utf8"})).realise(function (err, data) {
+        b_.stream(fs.ReadStream("./test/badfilename", {encoding:"utf8"})).realise(function (data) {
             test.ok(data[0] instanceof Error);
             test.done();
         });
@@ -116,7 +116,7 @@ module.exports = {
     "Read URL Stream returns contents":function (test) {
         var req = http.request(url.parse("http://www.google.com"), function (res) {
             res.setEncoding('utf8');
-            b_.stream(res).realise(function (err, data) {
+            b_.stream(res).realise(function (data) {
                 test.ok(data);
                 test.done();
             });
@@ -146,10 +146,10 @@ function allways() {
 function dummyIterator() {
     return {
         hasNext: function(callback) {
-            callback(null, true);
+            callback(true);
         },
         next: function(callback) {
-            callback(null, 1);
+            callback(1);
         }
     }
 }
