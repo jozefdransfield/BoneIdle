b72b680319b149fcdb126e1fdac0cdc416ca7184
Jozef Dransfield <Jozef.Dransfield@me.com>
2012-06-20 00:52:25 +0100

    Chain now passes through an option

--- test/chain_tests.js
+++ test/chain_tests.js
@@ -3,37 +3,38 @@ var b_ = require("./../lib/boneidle");
 module.exports = {
     "Chain callbacks test":function (test) {
         var chain = b_.chain(isNotNull).and(hasLengthGreaterThan2);
-        chain.call("my param", function (either) {
-            test.ok(either.right());
+        chain.call("my param", function (option) {
+            test.ok(!option.isEmpty());
             test.done();
         });
     },
     "Chain fails first callback":function (test) {
         var chain = b_.chain(isNotNull).and(hasLengthGreaterThan2);
-        chain.call("s", function (either) {
-            test.ok(either.isLeft());
+        chain.call("s", function (option) {
+            test.ok(option.isEmpty());
             test.done();
         });
     },
     "Chain fails second callback":function (test) {
         var chain = b_.chain(isNotNull).and(hasLengthGreaterThan2);
-        chain.call(null, function (either) {
-            test.ok(either.isLeft());
+        chain.call(null, function (option) {
+            test.ok(option.isEmpty());
             test.done();
         });
     }
 }
 function isNotNull(value, callback) {
     if (value) {
-        callback(b_.either.right(value));
+        callback(b_.option.some(value));
     } else {
-        callback(b_.either.left(value));
+        callback(b_.option.none());
     }
 }
 function hasLengthGreaterThan2(value, callback) {
     if (value.length > 2) {
-        callback(b_.either.right(value));
+        callback(b_.option.some(value));
     } else {
-        callback(b_.either.left(value));
+        callback(b_.option.none());
     }
 }
+
