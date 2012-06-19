69a8b782811b07b397af3b97389b87c8d98b84e9
Jozef Dransfield <Jozef.Dransfield@me.com>
2012-06-19 23:08:45 +0100

    fixed test name

--- test/pair_tests.js
+++ test/pair_tests.js
@@ -1,7 +1,7 @@
 var b_ = require("./../lib/boneidle");
 
 module.exports = {
-    "Name And Value":function (test) {
+    "Pair Test":function (test) {
         var nv = b_.pair("a name", "a value");
         test.same(nv.first, "a name");
         test.same(nv.second, "a value");
